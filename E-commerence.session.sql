f78db8c0fa645fa2ab5b6d6d5cdd42406193aaed
Ashley Giselle Lorenzo <92343168+ashleylozz02@users.noreply.github.com>
2022-07-27 08:34:06 -0400

    Update E-commerence.session.sql

--- E-commerence.session.sql
+++ E-commerence.session.sql
@@ -2,3 +2,14 @@ ALTER TABLE `CRUD`.`Products`
 CHANGE COLUMN `Primary Key` `Primary Key` VARCHAR(45) NOT NULL ,
 DROP PRIMARY KEY,
 ADD PRIMARY KEY (`idProducts`, `Primary Key`);
+USE CRUD ;
+
+CREATE table products(
+id Products INT AUTO_INCREMENT,,
+number INT,
+name VARCHAR(45),
+type VARCHAR(45),
+price INT,
+price INT,
+  Item description VARCHAR (150),
+  );
